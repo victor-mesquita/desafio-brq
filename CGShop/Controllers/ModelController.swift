63c85e77c39ead2ec1872b8c2107d2e460388bca
Admin <admin@nb023701.brq.com>
2018-07-17 17:58:57 -0300

    Mudanças na estrutura de pastas

---
+++ CGShop/Controllers/ModelController.swift
@@ -0,0 +1,28 @@
+//
+//  ModelController.swift
+//  CGShop
+//
+//  Created by mac on 16/07/2018.
+//  Copyright © 2018 cg. All rights reserved.
+//
+
+import UIKit
+
+/*
+ A controller object that manages a simple model -- a collection of month names.
+ 
+ The controller serves as the data source for the page view controller; it therefore implements pageViewController:viewControllerBeforeViewController: and pageViewController:viewControllerAfterViewController:.
+ It also implements a custom method, viewControllerAtIndex: which is useful in the implementation of the data source methods, and in the initial configuration of the application.
+ 
+ There is no need to actually create view controllers for each page in advance -- indeed doing so incurs unnecessary overhead. Given the data model, these methods create, configure, and return a new view controller on demand.
+ */
+
+
+class ModelController: NSObject {
+
+
+    override init() {
+        super.init()
+    }
+}
+
---
+++ CGShop/Controllers/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RootViewController.swift
+//  CGShop
+//
+//  Created by mac on 16/07/2018.
+//  Copyright © 2018 cg. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController, UIPageViewControllerDelegate {
+
+    var pageViewController: UIPageViewController?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
