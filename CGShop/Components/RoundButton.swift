b19228c419400116c0b302f8171f41a6bdb508c3
Victor Mesquita <skovhm@gmail.com>
2019-01-05 22:35:16 -0200

    :WIP: Adicionar ao carrinho

---
+++ CGShop/Components/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  CGShop
+//
+//  Created by Victor Mesquita on 05/01/2019.
+//  Copyright © 2019 cg. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
